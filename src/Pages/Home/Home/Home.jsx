0c18a463b3b0d4a29892836a9c35281a0713e82a
hasanalam24 <hasanalam365@gmail.com>
2024-07-05 01:01:37 +0600

    new arrival down images

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,24 @@
 import Banner from "../Banner/Banner";
 import NewArrival from "../NewArrival/NewArrival";
+import PopularProducts from "../PopularProducts/PopularProducts";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <NewArrival></NewArrival>
+
+
+            <div className="flex flex-col md:flex-row lg:flex-row gap-5 p-4">
+                <div className="flex-1">
+                    <img className="h-[300px] md:h-[300px] lg:h-[400px] w-full rounded-lg" src="https://i.ibb.co/JmS84pt/home-down-2.jpg" alt="" />
+                </div>
+                <div className="flex-1">
+                    <img className="h-[300px] md:h-[300px] lg:h-[400px] w-full rounded-lg" src="https://i.ibb.co/m9XYrXP/home-down-1.jpg" alt="" />
+                </div>
+            </div>
+
+            <PopularProducts></PopularProducts>
         </div>
     );
 };
