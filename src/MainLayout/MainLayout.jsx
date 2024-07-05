6f1b493018f29787f165756437984fd28b3ff95c
hasanalam24 <hasanalam365@gmail.com>
2024-07-05 19:57:58 +0600

    footer done

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navber from "../Shared/Navber/Navber";
+import Footer from "../Shared/Footer/Footer";
 
 
 const MainLayout = () => {
     return (
-        <div className="container mx-auto">
-            <Navber></Navber>
-            <Outlet></Outlet>
+        <div>
+            <div className="container mx-auto">
+                <Navber></Navber>
+                <Outlet></Outlet>
+
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
